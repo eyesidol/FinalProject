4176eedfe8af76766c979f843a93583da29bb99a
Andrew <milnera92@outlook.com>
2022-11-24 12:48:16 -0500

    backend tested and ready for real code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import {useState, useEffect} from "react"
-
-
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [message, setMessage] = useState("Test Failed No Message");
+  const [secondMessage, setSecondMessage] = useState("Handler Test Failed");
 
+  // test fetch, NO handler
   useEffect(() => {
     fetch("/test").then((res) =>
       res
@@ -14,9 +14,22 @@ const App = () => {
     );
   }, []);
 
+  // test fetch, WITH handler
+  useEffect(() => {
+    fetch("/test-message").then((res) =>
+      res
+        .json()
+        .then((data) => setSecondMessage(data.data))
+        .catch((e) => console.log("Error, no message", e))
+    );
+  }, []);
+
   return (
     <div>
       <h1>Test</h1>
+      <p>{message}</p>
+      <h1>Handler Test</h1>
+      <p>{secondMessage}</p>
     </div>
   );
 };
