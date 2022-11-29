6d6bf20d21c88c5fc14827c53f3453204d2b63fd
Andrew <milnera92@outlook.com>
2022-11-29 14:29:48 -0500

    styled components, global style

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Test from "./Test";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import GlobalStyle from "./GlobalStyles";
+
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import Profile from "./Profile";
@@ -13,6 +15,7 @@ import Footer from "./Footer";
 const App = () => {
   return (
     <BrowserRouter>
+    <GlobalStyle/>
     <Header/>
       <Routes>
         <Route path="/:id" element={<Homepage  />} />
