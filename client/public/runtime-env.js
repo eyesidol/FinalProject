6f00920ef15da6e77b40b55859d939dc1d096885
Andrew Milner <95140821+eyesidol@users.noreply.github.com>
2022-12-05 14:22:41 -0500

    Update runtime-env.js
    removed all api keys

--- client/public/runtime-env.js
+++ client/public/runtime-env.js
@@ -1,4 +1,1 @@
-window.__RUNTIME_CONFIG__ = {"NODE_ENV":"development","API_URL":"https://my-awesome-api.com", 
-"REACT_APP_AUTH0_DOMAIN": "dev-6cdvbdwrpi1z7vgp.us.auth0.com",
-"REACT_APP_AUTH0_CLIENT_ID": "D32gB50pC9DCwH17PZYEzwVHDDcXAwUZ",
-"REACT_APP_MAPS": "AIzaSyA09vo3TfiiSL2Asv5aHr5NfZYO2Pxd5A4"};
+window.__RUNTIME_CONFIG__ = {"REMOVED ALL": "REMOVED ALL API KEYS"};
